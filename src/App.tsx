2db988008999dd5f6123bc3b8f3017c937364b8c
Oscar Ossa <oskrlex17@gmail.com>
2024-04-03 16:29:35 -0500

    feat: add header and search form component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { ApiResponseProvider } from "@/context/ApiResponseContext";
+import SearchForm from "@/components/SearchForm/SearchForm";
+import Logo from "@/assets/images/logo.png"
 
 function App() {
   return (
     <ApiResponseProvider>
-      <p>Hello World</p>
+      {/* Header */}
+      <nav className="fixed top-0 left-0 w-full z-50 flex justify-center md:justify-between lg:justify-between xl:justify-between items-center p-4 bg-white bg-opacity-90 shadow-lg">
+        <img src={Logo} alt="Red Planet Logo" width={200} height={32} />
+      </nav>
+      {/* Search Form */}
+      <SearchForm />
     </ApiResponseProvider>
-  )
+  );
 }
 
-export default App
+export default App;
