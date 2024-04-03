510db8247436f372fb12df71b74c84bb44470e89
Oscar Ossa <oskrlex17@gmail.com>
2024-04-03 15:52:31 -0500

    feat: Implement function to fetch API data with error handling

---
+++ src/api/ApiClient.ts
@@ -0,0 +1,27 @@
+import { ApiResponse } from "@/types/ApiResponse";
+
+export const fetchApiData = async (endpoint: string, param: string): Promise<ApiResponse | null> => {
+  try {
+    const token = "YOUR_ACCESS_TOKEN"
+
+    const response = await fetch(
+      `http://194.140.197.189:8090/api/v1/${endpoint}/${param}`,
+      {
+        headers: {
+          "Api-Token": token,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text(); // Capture error details if available
+      throw new Error(`API Error (${response.status}): ${errorText || 'Unknown error'}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    
+    return null;
+  }
+};
