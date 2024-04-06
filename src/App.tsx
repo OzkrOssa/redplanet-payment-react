38143f792eae5d93e154f109421572483e81677c
Oscar Ossa <oskrlex17@gmail.com>
2024-04-06 15:26:52 -0500

    feat: Add Paymentez context provider to access a tokenized card token

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { ApiResponseProvider } from "@/context/ApiResponseContext";
+import { ApiResponseProvider, PaymentezSdkResponseProvider } from "@/context";
 import SearchForm from "@/components/SearchForm/SearchForm";
-import Logo from "@/assets/images/logo.png"
+import Logo from "@/assets/images/logo.png";
 
 function App() {
   return (
-    <ApiResponseProvider>
-      {/* Header */}
-      <nav className="fixed top-0 left-0 w-full z-50 flex justify-center md:justify-between lg:justify-between xl:justify-between items-center p-4 bg-white bg-opacity-90 shadow-lg">
-        <img src={Logo} alt="Red Planet Logo" width={200} height={32} />
-      </nav>
-      {/* Search Form */}
-      <SearchForm />
-    </ApiResponseProvider>
+    <PaymentezSdkResponseProvider>
+      <ApiResponseProvider>
+        {/* Header */}
+        <nav className="fixed top-0 left-0 w-full z-50 flex justify-center md:justify-between lg:justify-between xl:justify-between items-center p-4 bg-white bg-opacity-90 shadow-lg">
+          <img src={Logo} alt="Red Planet Logo" width={200} height={32} />
+        </nav>
+        {/* Search Form */}
+        <SearchForm />
+      </ApiResponseProvider>
+    </PaymentezSdkResponseProvider>
   );
 }
 
