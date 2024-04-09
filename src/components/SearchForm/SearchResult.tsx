6b909828e5b7bfeb545a7250298c76564a379128
Oscar Ossa <oskrlex17@gmail.com>
2024-04-09 15:09:29 -0500

    add: implement loader and payment status message

--- src/components/SearchForm/SearchResult.tsx
+++ src/components/SearchForm/SearchResult.tsx
@@ -2,8 +2,15 @@ import formatDate from "@/lib/formatDate";
 import { ApiResponse } from "@/types/ApiResponse";
 import React from "react";
 import { PaymentOptions } from "@/components/PaymentOptions";
+import { useGlobalPaymentResponseContext } from "@/context";
+import { TailSpin } from "react-loader-spinner";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Rocket } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 function SearchResults({ data }: { data: ApiResponse }) {
+  const { globalPaymentResponse, loading } = useGlobalPaymentResponseContext();
+
   return (
     <React.Fragment>
       {data.bill_number ? (
@@ -41,7 +48,65 @@ function SearchResults({ data }: { data: ApiResponse }) {
               </div>
             </div>
           </div>
-          <PaymentOptions/>
+          <div className="m-10 flex flex-col items-center">
+            {/* Spinner de carga */}
+            {loading && (
+              <TailSpin
+                visible={true}
+                height="50"
+                width="50"
+                color="#5e1774"
+                ariaLabel="tail-spin-loading"
+                radius="1"
+              />
+            )}
+            
+            {/* Renderizar la alerta según la respuesta del pago */}
+            {globalPaymentResponse &&
+              (globalPaymentResponse?.transaction.status !== "success" ? (
+                // Si el estado no es "approved"
+                <Alert className="text-xl">
+                  <Rocket className="h-5 w-5" />
+                  <AlertTitle>Pago procesado sin exito</AlertTitle>
+                  <AlertDescription>
+                    <div className="flex justify-evenly p-2">
+                      <p>Estado del pago: </p>
+                      <Badge variant={"outline"} className="bg-red-500">
+                        {globalPaymentResponse.transaction.status}
+                      </Badge>
+                    </div>
+                    <div className="flex justify-evenly">
+                      ID del pago:
+                      <Badge variant={"outline"}>
+                        {globalPaymentResponse.transaction.id}
+                      </Badge>
+                    </div>
+                    <p>Contacte con el area de facturación</p>
+                  </AlertDescription>
+                </Alert>
+              ) : (
+                // Si el estado es "approved"
+                <Alert className="text-xl">
+                  <Rocket className="h-5 w-5" />
+                  <AlertTitle>Pago procesado con exito</AlertTitle>
+                  <AlertDescription>
+                    <div className="flex justify-evenly p-2">
+                      <p>Estado del pago: </p>
+                      <Badge variant={"outline"} className="bg-green-400">
+                        {globalPaymentResponse.transaction.status}
+                      </Badge>
+                    </div>
+                    <div className="flex justify-evenly">
+                      ID del pago:
+                      <Badge variant={"outline"}>
+                        {globalPaymentResponse.transaction.id}
+                      </Badge>
+                    </div>
+                  </AlertDescription>
+                </Alert>
+              ))}
+          </div>
+          <PaymentOptions />
         </>
       ) : (
         <div className="text-3xl font-semibold text-red-500 mt-10 ">
